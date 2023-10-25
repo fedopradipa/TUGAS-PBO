7425b8d1672f2cb21460a00c8883530d357ea95f
fedopradipa <134703642+fedopradipa@users.noreply.github.com>
2023-10-25 16:04:01 +0700

    Add files via upload

---
+++ PERTEMUAN1/kubus.py
@@ -0,0 +1,15 @@
+print("program menghitung luas dan volume kubus")
+
+# Atur nilai variabel
+rusuk = int(input("masukan niai ="))
+
+
+# Menghitung luas permukaan kubus
+luas = 6 * rusuk ** 2
+# menghitung volume kubus
+volume = rusuk ** 3
+
+# Output hasil perhitungan
+print("---hasil perhitungan---")
+print(f"luas permukaan kubus adalah: {luas}")
+print(f"volume kubus adalah: {volume}")
